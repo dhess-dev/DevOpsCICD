f32d7520deecd0e44a091ce0987dcaa45dc4e9a1
dhess-dev <danielhess.dev@gmail.com>
2026-01-21 12:15:07 +0100

    fix: update .NET version in CI/CD workflow from 8.0.x to 10.0.x

---
+++ src/BlazorWebApp/Utils/MathUtils.cs
@@ -0,0 +1,6 @@
+namespace BlazorWebApp.Utils;
+
+public static class MathUtils
+{
+    public static int Add(int a, int b) => a + b;
+}
--- src/TestProject/UnitTest1.cs
+++ src/TestProject/UnitTest1.cs
@@ -1,9 +1,14 @@
-﻿namespace TestProject;
+﻿using BlazorWebApp.Utils;
+using Xunit;
+
+namespace TestProject;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Test_Add_ReturnsSum()
     {
+        var result = MathUtils.Add(2, 3);
+        Assert.Equal(5, result);
     }
 }
